115ba7ad954af7135aa27c45d4b0daa04847ee64
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2023-12-22 19:25:09 -0300

    fix: document and comment App

--- frontend/src/app/layout/App.js
+++ frontend/src/app/layout/App.js
@@ -3,11 +3,23 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "../context/AuthContext";
 import Routes from "../router/Routes";
 
+/**
+ * Main App component that sets up the application's structure.
+ *
+ * This component serves as the root component for the application, wrapping
+ * the entire application with BrowserRouter for routing and AuthProvider for
+ * managing authentication state.
+ *
+ * @returns {JSX.Element} The JSX representation of the App component.
+ */
 function App() {
   return (
     <>
+      {/* BrowserRouter provides routing capabilities for the application */}
       <BrowserRouter>
+        {/* AuthProvider manages the authentication state for its children */}
         <AuthProvider>
+          {/* Routes component handles rendering components based on routes */}
           <Routes />
         </AuthProvider>
       </BrowserRouter>
@@ -15,4 +27,5 @@ function App() {
   );
 }
 
+// Export the App component for use in the application
 export default App;
