3b34feb03b35c037b49ed98393ff947364bee991
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2023-12-22 19:11:26 -0300

    fix: document and comment api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,21 +9,21 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
+| routes are loaded by the RouteServiceProvider, and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
 */
 
+// Get the authenticated user information
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('login', [\App\Http\Controllers\auth\LoginController::class, 'login']);
+// User Authentication (Login)
+Route::post('login', [\App\Http\Controllers\Auth\LoginController::class, 'login']);
 
-Route::get('users', [\App\Http\Controllers\UserController::class, 'index']);
-
-Route::post('users', [\App\Http\Controllers\UserController::class, 'store']);
-
-Route::put('users/{identification}', [\App\Http\Controllers\UserController::class, 'update']);
-
-Route::delete('users/{identification}', [\App\Http\Controllers\UserController::class, 'destroy']);
+// CRUD Operations for Users
+Route::get('users', [\App\Http\Controllers\UserController::class, 'index']);   // Get all users
+Route::post('users', [\App\Http\Controllers\UserController::class, 'store']);  // Create a new user
+Route::put('users/{identification}', [\App\Http\Controllers\UserController::class, 'update']); // Update user by identification
+Route::delete('users/{identification}', [\App\Http\Controllers\UserController::class, 'destroy']); // Delete user by identification
