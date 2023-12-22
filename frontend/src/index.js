251d4447320fb31ee89cb82732a42d3350e3abe9
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2023-12-22 19:35:32 -0300

    fix: document and comment index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,16 @@
+/**
+ * React entry point for rendering the application.
+ * Uses React StrictMode for additional development checks.
+ */
+
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/layout/App";
 
+// Creating a root instance for rendering
 const root = createRoot(document.getElementById("root"));
 
+// Rendering the application within React StrictMode
 root.render(
   <StrictMode>
     <App />
