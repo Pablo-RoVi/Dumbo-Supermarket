41d0eef2d6e9e6ad04c7a2264c95f82940f3f500
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2023-12-22 19:24:06 -0300

    fix: document and comment AuthContext

--- frontend/src/app/context/AuthContext.js
+++ frontend/src/app/context/AuthContext.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from "react";
 
+// Create an authentication context with default values
 const AuthContext = createContext({
   authenticated: false,
   setAuthenticated: () => {},
@@ -7,10 +8,29 @@ const AuthContext = createContext({
   setToken: () => {},
 });
 
+/**
+ * AuthProvider component for managing authentication state.
+ *
+ * This component provides the AuthContext to its children and manages
+ * the state related to authentication, including 'authenticated' and 'token'.
+ *
+ * @param {object} props - React component properties.
+ * @param {React.ReactNode} props.children - The child components to be wrapped by the AuthProvider.
+ * @returns {JSX.Element} The JSX representation of the AuthProvider component.
+ */
 const AuthProvider = ({ children }) => {
+  // State to track the authentication status
   const [authenticated, setAuthenticated] = useState(false);
+
+  // State to store the authentication token
   const [token, setToken] = useState("");
 
+  /**
+   * AuthContext.Provider is used to provide the authentication context
+   * and the associated values to its children components.
+   * The 'value' prop contains the current values of 'authenticated', 'setAuthenticated',
+   * 'token', and 'setToken'.
+   */
   return (
     <AuthContext.Provider
       value={{ authenticated, setAuthenticated, token, setToken }}
@@ -20,4 +40,5 @@ const AuthProvider = ({ children }) => {
   );
 };
 
+// Export the AuthContext and AuthProvider for use in the application
 export { AuthContext, AuthProvider };
