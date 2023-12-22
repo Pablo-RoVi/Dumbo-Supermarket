088cc1b771f02f85902eb46af60a38ee56a9f5c3
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2023-12-22 19:18:23 -0300

    fix: document and comment Navbar

--- frontend/src/app/components/Navbar.js
+++ frontend/src/app/components/Navbar.js
@@ -6,10 +6,26 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
+/**
+ * Navbar component for the application.
+ *
+ * This component displays the navigation bar with a logo, brand name,
+ * and a "Cerrar Sesión" (Logout) button.
+ *
+ * @returns {JSX.Element} The JSX representation of the Navbar component.
+ */
 const Navbar = () => {
+  // Access the authentication context
   const { setAuthenticated } = useContext(AuthContext);
+
+  // Access the navigate function from react-router-dom
   const navigate = useNavigate();
 
+  /**
+   * Handles the logout action.
+   * Removes the token from localStorage, sets authentication to false,
+   * and navigates the user to the home page.
+   */
   const handleLogout = () => {
     localStorage.removeItem("token");
     setAuthenticated(false);
@@ -19,12 +35,17 @@ const Navbar = () => {
   return (
     <nav className="container-fluid navbar navbar-expand-lg navbar-primary-green">
       <div className="container" style={{ width: "100%" }}>
+        {/* Application Logo */}
         <img
           src="https://vignette.wikia.nocookie.net/logopedia/images/8/8e/Logo-jumbocencosud.png/revision/latest?cb=20160411225506"
           style={{ width: "7vh", height: "7vh", marginRight: "1%" }}
-          alt=""
+          alt="Application Logo"
         />
+
+        {/* Application Brand Name */}
         <a className="navbar-brand">DUMBO</a>
+
+        {/* Logout Button */}
         <button
           className="nav-link active"
           aria-current="page"
@@ -37,4 +58,5 @@ const Navbar = () => {
   );
 };
 
+// Export the Navbar component for use in the application
 export default Navbar;
